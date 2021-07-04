e3441ee33eab81a52572448b02ccf2b4941a35f4
seikin123 <abc521fjr7@gmail.com>
2021-07-04 01:42:50 +0000

    [add]ルートパスの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  get 'homes/top'
-  get 'homes/about'
-  get 'top/about'
-  devise_for :users, controllers: {
-  sessions:      'users/sessions',
-  passwords:     'users/passwords',
-  registrations: 'users/registrations'
+devise_for :users, controllers: {
+sessions:      'users/sessions',
+passwords:     'users/passwords',
+registrations: 'users/registrations'
 }
+
+root to: "homes#top"
   
 end
