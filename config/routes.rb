ccc22b3bd4120ca2c4eb3d04372ee4678742084f
seikin123 <abc521fjr7@gmail.com>
2021-07-05 14:36:50 +0000

    [fix]ヘッダーの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
-devise_for :users, controllers: {
-sessions:      'users/sessions',
-passwords:     'users/passwords',
-registrations: 'users/registrations'
-}
-
-root to: "homes#top"
-get 'about' => 'homes#about'
-
-devise_for :admins, controllers: {
-  sessions: 'admins/sessions'
-}
+  devise_for :users, controllers: {
+  sessions:      'users/sessions',
+  passwords:     'users/passwords',
+  registrations: 'users/registrations'
+  }
+  
+  root to: "homes#top"
+  get 'about' => 'homes#about'
+  
+  
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions'
+  }
   
 end
