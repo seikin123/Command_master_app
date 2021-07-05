00b80db4c344f92a4cb8c40611944f4ccc83cd29
seikin123 <abc521fjr7@gmail.com>
2021-07-05 15:37:16 +0000

    [add]userストロングパラメーター追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,14 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = current_user
   end
 
   def edit
+  end
+  
+ private
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :is_active, :level, :experience_point)
   end
 end
