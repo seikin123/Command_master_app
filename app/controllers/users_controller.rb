c19ba06d3335ed610436038dbdab0317042aa833
seikin123 <abc521fjr7@gmail.com>
2021-07-06 02:42:44 +0000

    [add]user/updateアクション追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,14 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   
-  
+  def updated
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+    redirect_to user_path(@user.id),notice: 'successfully updated!'
+    else
+      render :edit
+    end
+  end
   
  private
   def user_params
