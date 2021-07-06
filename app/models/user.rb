0c310465fcafc5d9544cb8eb289eb276b4797a37
seikin123 <abc521fjr7@gmail.com>
2021-07-06 03:36:42 +0000

    [add]退会済みアカウントでログインできないように制約

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
   attachment :profile_image
+  
+  # 
+  def active_for_authentication?
+    super && (self.is_active == true)
+  end
 end
