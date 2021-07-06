d0d755d9ea1d95babc1ee041fce290bcd43dd591
seikin123 <abc521fjr7@gmail.com>
2021-07-06 07:35:15 +0000

    [add]user_bot作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
+    name { 'redbull' }
+    email { 'redbull@gmail.com' }
+    password { 'redbull' }
+    password_confirmation { 'redbull' }
   end
 end
