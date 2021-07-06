905aea6dc38526163baad2d8c16dbcc25ca49ff4
seikin123 <abc521fjr7@gmail.com>
2021-07-06 04:19:05 +0000

    [fix]ログイン後の画面遷移をトップ画面へ変更

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,6 +3,9 @@
 class Users::SessionsController < Devise::SessionsController
   before_action :reject_user, only: [:create]
 
+  def after_sign_in_path_for(resource)
+    root_path
+  end
   # GET /resource/sign_in
   # def new
   #   super
