8dbc7ebbb7d5e422953dbd8cbe4122a86ad3642b
seikin123 <abc521fjr7@gmail.com>
2021-07-06 03:41:14 +0000

    [add]退会(unsubscribe/withdraw)のルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,16 @@ passwords:     'users/passwords',
 registrations: 'users/registrations'
 }
 
-resources :users
-
 root to: "homes#top"
 get 'about' => 'homes#about'
+
+resources :user, only: [:edit, :update, :show] do
+  collection do #idは付与しない
+    get 'unsubscribe' #退会画面
+    patch 'withdraw' #is_asciveを更新する
+  end
+end
+
+
   
 end
