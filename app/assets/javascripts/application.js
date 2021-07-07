0955c014ff2cf740a42ab36161e58e25d004c4cd
seikin123 <abc521fjr7@gmail.com>
2021-07-07 10:03:13 +0000

    [add]jqueryの動作確認

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+//= require jquery 
+//= require jquery_ujs 
 //= require_tree .
-//= require jquery #追加
-//= require jquery_ujs #追加
+
