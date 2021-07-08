b733032dc4547eadbe80a7b54d0e9793f39163ab
seikin123 <abc521fjr7@gmail.com>
2021-07-08 14:31:56 +0000

    [add]json形式での問題を追加

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -38,7 +38,30 @@ let questions =  [{
   "shiftKey": true,
   "score": 10
 },
-
+{
+  "id": "4",
+  "category_id": "1",
+  "question_value": "Vscodeの一つ前に戻す処理際のショートカットキー はなんですか",
+  "answer": "z",
+  "shiftKey": true,
+  "score": 10
+},
+{
+  "id": "5",
+  "category_id": "1",
+  "question_value": "Vscodeの選択範囲を切り取りのショートカットキー はなんですか",
+  "answer": "x",
+  "shiftKey": true,
+  "score": 10
+},
+{
+  "id": "6",
+  "category_id": "1",
+  "question_value": "Vscodeの指定した行にジャンプのショートカットキー はなんですか",
+  "answer": "g",
+  "shiftKey": true,
+  "score": 10
+},
 ]
 
 let i = 0;
