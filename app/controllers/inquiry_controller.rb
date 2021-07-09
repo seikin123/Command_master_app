43bf2a93b572b00976e51e8adc0730123c40768e
seikin123 <abc521fjr7@gmail.com>
2021-07-09 18:22:36 +0000

    [add]inqury/indexに入力画面の表示

--- app/controllers/inquiry_controller.rb
+++ app/controllers/inquiry_controller.rb
@@ -1,8 +1,13 @@
 class InquiryController < ApplicationController
+  
   def index
+     # 入力画面を表示
+    @inquiry = Inquiry.new
+    render :action => 'index'
   end
 
   def confirm
+    
   end
 
   def thanks
