abb9d9737343593dae2f54aea91cbe9405c652c9
seikin123 <abc521fjr7@gmail.com>
2021-07-09 18:15:41 +0000

    [add]inquiryコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'inquiry/index'
+  get 'inquiry/confirm'
+  get 'inquiry/thanks'
 devise_for :users, controllers: {
 sessions:      'users/sessions',
 passwords:     'users/passwords',
