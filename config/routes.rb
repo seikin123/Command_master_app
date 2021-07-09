843b919bd354566c31257b8e0c33ded4f488a6af
seikin123 <abc521fjr7@gmail.com>
2021-07-09 18:30:44 +0000

    [add]inquryのルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,27 @@
 Rails.application.routes.draw do
-  get 'inquiry/index'
-  get 'inquiry/confirm'
-  get 'inquiry/thanks'
-devise_for :users, controllers: {
-sessions:      'users/sessions',
-passwords:     'users/passwords',
-registrations: 'users/registrations'
-}
+  devise_for :users, controllers: {
+  sessions:      'users/sessions',
+  passwords:     'users/passwords',
+  registrations: 'users/registrations'
+  }
 
-root to: "homes#top"
-get 'about' => 'homes#about'
-
-resources :users do
-  collection do #idは付与しない
-    get 'unsubscribe' #退会画面
-    patch 'withdraw' #is_aciveを更新する
+  root to: "homes#top"
+  get 'about' => 'homes#about'
+  
+  #お問い合わせ機能のルーティング
+  get   'inquiry'         => 'inquiry#index'     # 入力画面
+  post  'inquiry/confirm' => 'inquiry#confirm'   # 確認画面
+  post  'inquiry/thanks'  => 'inquiry#thanks'    # 送信完了画面
+  
+  resources :users do
+    collection do #idは付与しない
+      get 'unsubscribe' #退会画面
+      patch 'withdraw' #is_aciveを更新する
+    end
   end
-end
-
   
-  
+    
+    
   devise_for :admins, controllers: {
     sessions: 'admins/sessions'
   }
