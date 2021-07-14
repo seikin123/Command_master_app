8c4447d40649ba864404b940148d0e8299755639
seikin123 <abc521fjr7@gmail.com>
2021-07-14 03:45:20 +0000

    [add]id4の問題の追加

--- db/fixtures/01_question.rb
+++ db/fixtures/01_question.rb
@@ -2,25 +2,13 @@
 Question.seed(:id,
  { id: 1, user_id: 1, category_id: 1, problem: 'コピーする際のコマンド', answer_key: 'c', pc_type: 0 },
  { id: 2, user_id: 1, category_id: 1, problem: '全選択をする際のコマンド', answer_key: 'a', pc_type: 0 },
- { id: 3, user_id: 1, category_id: 1, problem: 'ペーストする際のコマンド', answer_key: 'a', pc_type: 0 },
+ { id: 3, user_id: 1, category_id: 1, problem: 'ペーストする際のコマンド', answer_key: 'v', pc_type: 0 },
+ { id: 4, user_id: 1, category_id: 1, problem: '一つ前に戻す処理際のコマンド', answer_key: 'z', pc_type: 0 },
+ 
 )
 
-# {
-#   "id": "3",
-#   "category_id": "1",
-#   "question_value": "Vscodeのペーストする際のショートカットキー",
-#   "answer": "v",
-#   "shiftKey": true,
-#   "score": 10
-# },
-# {
-#   "id": "4",
-#   "category_id": "1",
-#   "question_value": "Vscodeの一つ前に戻す処理際のショートカットキー",
-#   "answer": "z",
-#   "shiftKey": true,
-#   "score": 10
-# },
+
+
 # {
 #   "id": "5",
 #   "category_id": "1",
