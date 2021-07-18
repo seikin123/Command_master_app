2bf88d1fc192a27af1a6f340221ba0b56e8dc243
seikin123 <abc521fjr7@gmail.com>
2021-07-18 12:21:49 +0000

    [add]同時押しのキーを保存するカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_11_102927) do
+ActiveRecord::Schema.define(version: 2021_07_17_112943) do
 
   create_table "admins", force: :cascade do |t|
     t.string "name"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2021_07_11_102927) do
     t.integer "pc_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "synchro_key"
   end
 
   create_table "users", force: :cascade do |t|
