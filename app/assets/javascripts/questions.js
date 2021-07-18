0ed3f5c6c4013871a33fab59b0f479abda52901a
seikin123 <abc521fjr7@gmail.com>
2021-07-18 12:19:51 +0000

    [fix]キーの同時判定に変更

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -1,101 +1,142 @@
-// 正解の答え
-// let json_question = {
-//   "key": "v",
-//   "ctrl": true,
-//   "meta": false,
-//   "alt": false,
-//   "shift": false
-// }
 
-// const $startMessage = $('#start-message');
-
-window.onload = function () {
+$(document).ready(function () {
   const Json = document.querySelector('#questions_json').value;
   const questions = JSON.parse(Json);
-  const answer_key = questions.answer_key;
-  // console.log(questions[0].answer_key);
   const pc = document.querySelector('#pc_type').value;
-  console.log(questions);
-  console.log(questions[0].answer_key);
-  console.log(questions[0].problem);
-}
-// キー判定の処理
+  const input = document.querySelector('#input');
+  const $timeMessage = $('#time-message'); 
+  
+  var i = 0; //問題文
+  var start_game = false;　//ゲームスタート
+  var start_time = 0; //時間の設定
+  /*$(document).on('keydown', function(){
     
+  })*/　　
+  var keys = [];
+  //終了判定
+  function finishAnswer() {
+    $(".finish").show();
+    const end_time = performance.now();
+    const typing_time = ( (end_time - start_time) / 1000).toFixed(2);
+    $timeMessage.text('かかった時間：'+typing_time+'秒');
+   }
+   
+   // キー判定の処理
   document.addEventListener("keydown", e => {
+    console.log(keys)
     e.preventDefault();
-    let i = 0;
-    let text = e.key;
-    let command = ' ';
-    if (e.ctrlKey){
-    text = command + text;
-    }
-    $("#input").text(text);
-    //入力されたキーが小文字でないと反応しないため、let event_key = e.key.toLowerCase();
-    //問題に答えのキー登録しておき、else ifで特殊キーが押されたらそのキーを表示させるようにする
-    if ((e.key === questions[0].answer_key && e.ctrlKey) || (e.key === questions[0].answer_key && e.metaKey)) {  //答えのキー
-      console.log("true");
-      // console.log(questions.answer_key);
-      $("#input").text('');
-    }
+    keys.push(e.key);
+    console.log(keys)
   });
+  document.addEventListener("keyup", e => {
+    if( keys.length > 2){
+      keys = [];
+      return false;
+    }else if(keys.length == 1){
+      if (!start_game && e.keyCode === 32) { 
+        $("#start-message").hide();
+        $('#questions').show();
+        $('#question-' + i).show();
+        start_game = true;
+        start_time = performance.now();
+    }else if(start_game){
+        $('#input').text(keys[0]);
+        if(keys[0] === questions[i].synchro_key) { //特殊キー
+          console.log(questions[i].synchro_key);
+          console.log("true" + k);
+          $("#input").text('');
+          $('#question-' + i).hide();
+              i += 1;
+          $('#question-' + i).show();
+        }
+      }
+      keys = [];
+    }else if(keys.length == 2 && start_game){ //答えのキーが二つの場合
+      $('#input').text(keys.join(' '));
+      keys.forEach(function(k){
+        if(k === questions[i].answer_key) { //2つ目の答え
+          console.log("true" + k);
+          $("#input").text('');
+          $('#question-' + i).hide();
+            i += 1;
+          $('#question-' + i).show();
+        if (questions.length <= i){ //問題終了判定
+          finishAnswer();
+        }
+        }
+      });
+      keys = [];
+    }else{
+      keys = [];
+    }
+});
 
-// jsのみの挙動
-// let start_game = false;
-// let ans = 0;
-// let start_time = 0; // <<--ゲームがスタートしたかを判断する変数
 
 
-// $(document).ready(function () {
-//   console.log("document.ready")
 
-//   function changeText(){
-//     $("#question").text(questions[i].question_value);
-//     $("#ans").text(ans);
-//   };
-//   function addScore(score){
-//     ans += score;
-//   };
-//   function test(test){
-//     console.log()
-//   };
-//   function finishAnswer(){
-//     $("#question").text('');
-//     $("#ans").text('');
-//     $(".finish").show();
-//   };
+    /*keys.forEach(function(k){
+      if (!start_game && e.keyCode === 32) { 
+        $("#start-message").hide();
+        $('#questions').show();
+        $('#question-' + i).show();
+        start_game = true;
+      // start_time = performance.now();
+            return;
+      } else if (!start_game) {
+        return;
+      }*/
+      /*let text = e.key;
+      let command = ' ';
+      if (e.ctrlKey || e.metakey){
+          text = command + text;
+      }*/
+      /*$("#input").text(text);
+      console.log(i)
+      if (event.metaKey && e.key === questions[i].answer_key) {
+          console.log("true");
+          $("#input").text('');
+          $('#question-' + i).hide();
+          i += 1;
+          $('#question-' + i).show();
+        }
+    });*/
+  /*document.addEventListener("keydown", e => {
+      e.preventDefault();
+      console.log(e.key)
+      if (!start_game && e.keyCode === 32) { 
+      $("#start-message").hide();
+      $('#questions').show();
+      $('#question-' + i).show();
+        start_game = true;
+      // start_time = performance.now();
+            return;
+          } else if (!start_game) {
+            return;
+          }
+      let text = e.key;
+      let command = ' ';
+      if (e.ctrlKey || e.metakey){
+          text = command + text;
+      }
+      $("#input").text(text);
+      console.log(i)
+      if (event.metaKey && e.key === questions[i].answer_key) {
+          console.log("true");
+          $("#input").text('');
+          $('#question-' + i).hide();
+          i += 1;
+          $('#question-' + i).show();
+        }
+    });*/
+});
 
-//   changeText();
-//   document.addEventListener("keydown", e => {
-//     e.preventDefault(); //ブラウザでのキーの反応を防ぐ
-//     if (!start_game && e.keyCode === 32) { 
-//       $("#start-message").hide();
-//       $("#question").show();
-//       $("#ans").show();
-//       start_game = true;
-//       start_time = performance.now();
-//       return;
-//     } else if (!start_game) {
-//       return;
-//     }
-//     console.log(e);
-    // let text = e.key;
-    // let command = ' ';
-    // if (e.ctrlKey){
-    //   text = command + text;
-    // }
-    // $("#input").text(text);
-    
-//     if ((e.key === questions[i].answer_key && e.ctrlKey) || (e.key === questions[i].answer_key && e.metaKey)) {  //答えのキー
-//       console.log("true");
-//       $("#input").text('');  //入力されたキーを表示
-//       addScore(questions[i].score);  //スコア加点
-//       i++;
-//       if (questions.length <= i){ //問題終了判定
-//         finishAnswer();
-//       } else {
-//         changeText();
-//       }
-//     }
-//   });
-// });
 
+    //正解 
+  // var synchro = { meta: '', ctrl: '', alt: '', shift: '' }
+    // Mac/windows切り替え
+    // window.onload = () => {
+    // synchro.meta  = (pc === 'Mac')? '<span id="meta"> ⌘ </span>'  : '<span id="meta"> Win </span>';
+    // synchro.ctrl  = (pc === 'Mac')? '<span id="ctrl"> ⌃ </span>'  : '<span id="ctrl"> Ctrl </span>';
+    // synchro.alt   = (pc === 'Mac')? '<span id="alt"> ⌥ </span>'   : '<span id="alt"> Alt </span>';
+    // synchro.shift = (pc === 'Mac')? '<span id="shift"> ⇧ </span>' : '<span id="shift"> Shift </span>';
+    // };
