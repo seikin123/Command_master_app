7f410f608b2d10b559642f8ecfebf0597d320202
seikin123 <abc521fjr7@gmail.com>
2021-07-19 08:02:09 +0000

    [add]likesテーブル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_17_112943) do
+ActiveRecord::Schema.define(version: 2021_07_19_075913) do
 
   create_table "admins", force: :cascade do |t|
     t.string "name"
@@ -44,6 +44,16 @@ ActiveRecord::Schema.define(version: 2021_07_17_112943) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "Question_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["Question_id"], name: "index_likes_on_Question_id"
+    t.index ["user_id", "Question_id"], name: "index_likes_on_user_id_and_question_id", unique: true
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
   create_table "questions", force: :cascade do |t|
     t.integer "category_id"
     t.integer "drill_id"
@@ -54,6 +64,7 @@ ActiveRecord::Schema.define(version: 2021_07_17_112943) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "synchro_key"
+    t.string "display_key"
   end
 
   create_table "users", force: :cascade do |t|
