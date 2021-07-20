38cbaaa69dcd7835a3825ffec47d1f2a20a38b6d
seikin123 <abc521fjr7@gmail.com>
2021-07-20 03:24:03 +0000

    [add]いいね機能の関連付け

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,13 @@
 class Question < ApplicationRecord
-  # has_many :answers
+  has_many :categories
+  #いいね機能
+  has_many :likes, dependent: :destroy
+  has_many :like_users, through: :likes, source: :user
+  
+  def like?(user)
+    like_users.include?(user)
+  end
+  
   enum pc_type: { Mac: 0, Windows: 1 }
 
   def questions(pc_type) #osの判定
