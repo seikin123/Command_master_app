f6392a3c17a4c0577e6e97d9119676973a8646c6
seikin123 <abc521fjr7@gmail.com>
2021-07-20 03:20:30 +0000

    [add]commandsコントローラー

---
+++ app/controllers/commands_controller.rb
@@ -0,0 +1,16 @@
+class CommandsController < ApplicationController
+  before_action :authenticate_user!
+  
+  def index
+    @questions = Question.new.questions(request.os)
+
+    @questions_json = @questions
+  end
+  
+  def show
+    @question = Question.find(params[:id])
+    # byebug
+    # @question_json = @question
+    # @pc_type = set_pc_type
+  end
+end
