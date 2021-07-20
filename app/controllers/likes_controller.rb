2d53283f45a21b544c5f9f4108de7944378df133
seikin123 <abc521fjr7@gmail.com>
2021-07-20 03:23:05 +0000

    [add]非同期通信によるいいね機能

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,20 @@
 class LikesController < ApplicationController
+  
+  def create
+    @question = Question.find(params[:uestion_id])
+    @like = current_user.likes.find_by(question: @question)
+    toggle
+  end
+  
+  private
+
+  def toggle
+    if @like
+      return head :unprocessable_entity unless @like.destroy
+    else
+      @like = current_user.likes.build(question: @question)
+      return head :unprocessable_entity unless @like.save
+    end
+    head :ok
+  end
 end
