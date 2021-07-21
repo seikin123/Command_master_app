086e72454ff09e377f6dfe86882edb1ce24f1371
seikin123 <abc521fjr7@gmail.com>
2021-07-21 17:40:04 +0000

    [add]レイアウト調整

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'cd0b924d8be4c4bfada45ec7b9dc3e6a9747098eeda26685b48e2dbf3eebbcd16e74a5691cfdefa235f041348be5de4a160c454e44fa7a8058488a87521c4459'
