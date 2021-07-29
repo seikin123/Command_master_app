0653138d566f003489af9a1e4a29cabbd3db9a1a
seikin123 <abc521fjr7@gmail.com>
2021-07-29 13:59:37 +0000

    [change]アニメーション追加

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -1,8 +1,8 @@
 //ローディング画面の表示
-// $(window).on('load',function(){
-//   $("#loading").delay(1500).fadeOut('slow');//ローディング画面を1.5秒（1500ms）待機してからフェードアウト
-//   $("#loading_box").delay(1400).fadeOut('slow');//ローディングテキストを1.2秒（1200ms）待機してからフェードアウト
-// });
+$(window).on('load',function(){
+  $("#loading").delay(1500).fadeOut('slow');//ローディング画面を1.5秒（1500ms）待機してからフェードアウト
+  $("#loading_box").delay(1400).fadeOut('slow');//ローディングテキストを1.2秒（1200ms）待機してからフェードアウト
+});
 
  $(document).ready(function () {
   if (!$('#questions_json').length) {
