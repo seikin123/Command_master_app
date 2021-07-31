ebe1b723ab794ddc88e757124cf9785c6523db5b
seikin123 <abc521fjr7@gmail.com>
2021-07-31 12:15:09 +0000

    [add]問題練習機能追加

--- app/controllers/commands_controller.rb
+++ app/controllers/commands_controller.rb
@@ -11,7 +11,7 @@ class CommandsController < ApplicationController
   end
 
   def show
-    @questions = Question.select_pc_type(request.os)
+    @question = Question.find(params[:id])
     # if params[:category_id].present?
     # # @question = Question.find(params[:id])
     # @category = Category.find(params[:category_id])
