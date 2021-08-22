e869c897063875101ba3e4650ca2b35092db8e8a
seikin123 <abc521fjr7@gmail.com>
2021-08-22 14:40:06 +0000

    [add]レベルアップ機能の追加

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -14,22 +14,5 @@ class QuestionsController < ApplicationController
     end
       @user_id = current_user
   end
-      # @products = @category.products
-      # index = 1
-      #   Answer.where(user_id: current_user)
-      # @question = Question.find(index)
-  
-
-  # def create
-  #   a = Answer.new()# answer_params
-  #   # Answerテーブルに答えを登録
-
-  #   # 新しい問題を取得する
-  #   index = 1
-  #   if params[:answer][:id] != nil
-  #     index = params[:answer][:id].to_i + 1
-  #   end
-  #   @question = Question.find(index)
-  # end
 
 end
