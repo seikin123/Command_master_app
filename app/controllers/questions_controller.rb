42e1cfcef94b94409b598b06432a9d3cf3363f6e
seikin123 <abc521fjr7@gmail.com>
2021-09-17 04:21:23 +0000

    [fix]RAND

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,8 +8,8 @@ class QuestionsController < ApplicationController
       @categories = Category.all
     if params[:category_id].present?
       @category = Category.find(params[:category_id])
-      # @questions_json = @category.questions.select_pc_type(request.os).order(Arel.sql('RAND()')).limit(10) #注意mysql=RAND()
-      @questions_json = @category.questions.select_pc_type(request.os).order(Arel.sql('RANDOM()')).limit(10) #注意mysql=RAND()
+      @questions_json = @category.questions.select_pc_type(request.os).order(Arel.sql('RAND()')).limit(10) #注意mysql=RAND()
+      # @questions_json = @category.questions.select_pc_type(request.os).order(Arel.sql('RANDOM()')).limit(10) #注意mysql=RAND()
     else
       redirect_to categories_path
     end
